e1dd4096e02cafd9933a00585698d6d95f4f7908
박기수 <rltn2121@naver.com>
2023-11-25 00:07:56 +0900

    gid 채번 api 내부연동 호출

--- src/main/java/core/apis/controller/MtcExgController.java
+++ src/main/java/core/apis/controller/MtcExgController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -34,6 +35,8 @@ public class MtcExgController implements MtcExgApi {
     public ResponseEntity<?> exchange(MtcExgRequest exgRequest) {
 
         MtcExgResponse exgResponse = new MtcExgResponse();
+        String gid = callGidApi();
+        log.info("@@@@@ gid: " + gid);
 
         try {
             // 충전 일련번호 채번 (아마 timestamp 를 붙이지 않을까 싶다)
@@ -55,5 +58,18 @@ public class MtcExgController implements MtcExgApi {
         }
 
         return ResponseEntity.ok(exgResponse);
+    }
+
+    private String callGidApi() {
+        return WebClient.create("http://mtc-com-log-svc.coc-mtc.svc.cluster.local:8080")
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/log/mkgid")
+                        .queryParam("svcid", "pay")
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 }
