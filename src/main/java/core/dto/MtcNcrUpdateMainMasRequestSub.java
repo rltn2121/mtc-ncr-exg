a5d19568dabb007258139e3b78f32c54663eb35d
gayoung <orz4120@gmail.com>
2023-11-25 14:15:38 +0900

    commit before pull

--- src/main/java/core/dto/MtcNcrUpdateMainMasRequestSub.java
+++ src/main/java/core/dto/MtcNcrUpdateMainMasRequestSub.java
@@ -1,5 +1,6 @@
 package core.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -7,6 +8,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MtcNcrUpdateMainMasRequestSub {
     private int sign; //부호 ( 입금이면 1 , 출금이면 -1)
     private double trxAmt; // 거래금액
